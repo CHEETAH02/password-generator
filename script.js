17b2e2aa16d94dcdd7b4c89e20452dc1fcb1bac0
Chetan Sathish <98352066+CHEETAH02@users.noreply.github.com>
2023-08-14 16:21:11 +0530

    Updated files

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
     var passLength = document.getElementById('passLength')
     var smallLetterCheckbox = document.getElementById('smallLetterCheckbox')
     var capLetterCheckbox = document.getElementById('capLetterCheckbox')
+    var numberCheckbox = document.getElementById('numberCheckbox')
     var specialCheckbox = document.getElementById('specialCheckbox')
     var submitButton = document.getElementById('submitButton')
     var textArea = document.getElementById('textArea')
@@ -11,15 +12,19 @@ window.addEventListener("DOMContentLoaded", () => {
     submitButton.addEventListener("click", function () {
         var charHeap = []
         var passString = []
-        if (smallLetterCheckbox.checked == true) {
+        if (smallLetterCheckbox.checked) {
             var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
             charHeap = charHeap.concat(arr)
         }
-        if (capLetterCheckbox.checked == true) {
+        if (capLetterCheckbox.checked) {
             var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
             charHeap = charHeap.concat(arr)
         }
-        if (specialCheckbox.checked == true) {
+        if (numberCheckbox.checked) {
+            var arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            charHeap = charHeap.concat(arr)
+        }
+        if (specialCheckbox.checked) {
             var arr = ['!', '@', '#', '$', '%', '^', '&', '*', '.', ',', '|', '=', '-', '_', '+']
             charHeap = charHeap.concat(arr)
         }
